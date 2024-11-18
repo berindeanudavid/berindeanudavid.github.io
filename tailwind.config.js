bcb7009c4000c4c7d0d3a0d47743e28b78e04ae3
berindeanudavid <david.berindeanu@emanuel.ro>
2024-11-18 18:41:00 +0200

    primii pasi

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+    './main.js',
+    
+  ],
+  // ...
+}
